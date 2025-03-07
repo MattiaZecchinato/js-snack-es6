f63bdf56696005c973c18e41a0df020a141ea165
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-07 14:12:35 +0100

    feat: create array of object snack1

--- snack1/script/main.js
+++ snack1/script/main.js
@@ -1,3 +1,29 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+// create array of object
+const arrayBike = [
+    {
+        nome: 'Pinarello Dogma F',
+        peso: 7
+    },
+    {
+        nome: 'Trek Madone SLR',
+        peso: 10
+    },
+    {
+        nome: 'Cervélo S5',
+        peso: 12
+    },
+    {
+        nome: 'Colnago V4Rs',
+        peso: 6
+    },
+    {
+        nome: 'Wilier Filante SLR',
+        peso: 15
+    }
+];
+
+console.log(arrayBike);
