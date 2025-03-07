8dc2eae6fe053613d1ccc77e053650ed59f336b1
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-07 14:56:41 +0100

    feat: create array of object snack2

--- snack2/script/main.js
+++ snack2/script/main.js
@@ -3,3 +3,33 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const footballTeam = [
+    {
+        teamName: 'juventus',
+        point: 0,
+        foulSuffered: 0
+    },
+    {
+        teamName: 'milan',
+        point: 0,
+        foulSuffered: 0
+    },
+    {
+        teamName: 'inter',
+        point: 0,
+        foulSuffered: 0
+    },
+    {
+        teamName: 'lazio',
+        point: 0,
+        foulSuffered: 0
+    },
+    {
+        teamName: 'roma',
+        point: 0,
+        foulSuffered: 0
+    }
+];
+
+console.log(footballTeam);
